51b394819a7bea37cb750ae79daa87bcafcbc3a4
Geovanny Hernandez <geohernandez@gmail.com>
2016-06-07 21:32:58 +0200

    Adding a DB for testing

---
+++ src/Demo_SqlUnitTesting/DeployDB_SimpleSQL.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE SimpleSQL
+GO
+
+--Pending to verify
+--Because we can use a typical DB with Sport Statistics
+CREATE TABLE Products
+(
+
+
+)
+
+
+
+CREATE TABLE Sales
+(
+
+
+)
+
+CREATE TABLE 
